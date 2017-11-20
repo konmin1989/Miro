3c87ce199b3dd15f0a9341c1885a3892109f7d6e
km.zhu <km.zhu@nf-3.com>
2017-11-20 18:09:46 +0800

    coding the layout for MiroActivity

--- miro/src/main/java/com/konmin/miro/ui/MiroActivity.java
+++ miro/src/main/java/com/konmin/miro/ui/MiroActivity.java
@@ -3,11 +3,12 @@ package com.konmin.miro.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.konmin.miro.R;
 
 /**
- * Describe
+ * the main interface for user select media;
  *
  * @author Konmin
  * @version create time:2017/11/20
