90c731540f70346a0c3e81eee88cd770d4feb79b
konmin <konmin1989@qq.com>
2017-11-20 23:17:36 +0800

    修改布局

--- miro/src/main/java/com/konmin/miro/ui/MiroActivity.java
+++ miro/src/main/java/com/konmin/miro/ui/MiroActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.konmin.miro.R;
 
@@ -21,5 +22,8 @@ public class MiroActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_miro);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.bar_title);
     }
 }
