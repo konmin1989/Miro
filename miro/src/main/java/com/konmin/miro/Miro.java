e9577c73c729e60d642e457b8be213799f163153
km.zhu <km.zhu@nf-3.com>
2017-11-20 14:25:42 +0800

    first commit

---
+++ miro/src/main/java/com/konmin/miro/Miro.java
@@ -0,0 +1,11 @@
+package com.konmin.miro;
+
+/**
+ * Entry for Miro's media picker
+ *
+ * @author Konmin
+ * @version create time:2017/11/20
+ */
+
+public class Miro {
+}
---
+++ miro/src/main/java/com/konmin/miro/entity/Album.java
@@ -0,0 +1,11 @@
+package com.konmin.miro.entity;
+
+/**
+ * the Album form media database
+ *
+ * @author Konmin
+ * @version create time:2017/11/20
+ */
+
+public class Album {
+}
